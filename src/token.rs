d3c26b7badc14d6137a31c9896848e5d0ac72ccb
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-01-23 15:30:53 +0400

    test yasdiq next tokeni almaga aid

--- src/token.rs
+++ src/token.rs
@@ -1,11 +1,13 @@
+use std::fmt::Display;
 
-struct Token {
-    kind: TokenKind,
-    value: String
+#[derive(PartialEq, Debug)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub literal: String,
 }
 
-
-enum TokenKind {
+#[derive(PartialEq, Debug)]
+pub enum TokenKind {
     Illegal,
     Eof,
 
@@ -24,6 +26,26 @@ enum TokenKind {
     Rbrace,
 
     Function,
-    Let
+    Let,
+}
 
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenKind::Illegal => write!(f, "Illegal"),
+            TokenKind::Eof => write!(f, "Eof"),
+            TokenKind::Ident => write!(f, "Ident"),
+            TokenKind::Int => write!(f, "Int"),
+            TokenKind::Assign => write!(f, "Assign"),
+            TokenKind::Plus => write!(f, "Plus"),
+            TokenKind::Comma => write!(f, "Comma"),
+            TokenKind::Semicolon => write!(f, "Semicolon"),
+            TokenKind::Lparen => write!(f, "Lparen"),
+            TokenKind::Rparen => write!(f, "Rparen"),
+            TokenKind::Lbrace => write!(f, "Lbrace"),
+            TokenKind::Rbrace => write!(f, "Rbrace"),
+            TokenKind::Function => write!(f, "Function"),
+            TokenKind::Let => write!(f, "Let"),
+        }
+    }
 }
